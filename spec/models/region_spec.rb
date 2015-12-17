ae09c3d2925850abb2e5116e550756e8cc70c7be
Hòa Nguyễn Văn <nvh0412@gmail.com>
2015-12-18 01:43:46 +0700

    [f] Implement spec for model

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_inclusion_of(:name).in_array(['Ho Chi Minh', 'Ha Noi', 'Binh Thuan', 'Da Nang', 'Lam Dong']) }
+  it { should validate_uniqueness_of(:name) }
 end
